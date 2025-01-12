c27e7dcf15d21379141d883ad64f4e3a87e06806
Amog Guntha <heythisisamog@gmail.com>
2025-01-12 21:36:39 +0530

    set up of client side routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-import NavBar from "./components/NavBar";
+import Body from "./components/Body";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LogIn from "./components/LogIn";
 function App() {
   return (
     <>
-      <NavBar />
-      <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<LogIn />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ frontend/src/components/LogIn.jsx
@@ -0,0 +1,9 @@
+const LogIn = () => {
+  return (
+    <>
+      <h1>Login page</h1>
+    </>
+  );
+};
+
+export default LogIn;
