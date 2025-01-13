bf589546603964e1eb56449a62fa6a8993016760
Amog Guntha <heythisisamog@gmail.com>
2025-01-13 10:53:48 +0530

    Added redux store to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import Body from "./components/Body";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LogIn from "./components/LogIn";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<LogIn />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<LogIn />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -1,8 +1,66 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import BASE_URL from "../utils/constants";
+
 const LogIn = () => {
+  const [emailId, setEmailId] = useState("heythisisamog@gmail.com");
+  const [password, setPassword] = useState("Bot@12345");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const onClickLogin = async () => {
+    const res = await axios.post(
+      BASE_URL + "/login",
+      {
+        emailId,
+        password,
+      },
+      { withCredentials: true }
+    );
+    dispatch(addUser(res.data));
+    navigate("/");
+  };
+
   return (
-    <>
-      <h1>Login page</h1>
-    </>
+    <div className="flex justify-center my-12">
+      <div className="card bg-base-300 w-96 shadow-xl h-[410px]">
+        <div className="card-body">
+          <h2 className="card-title mx-auto text-3xl text-white">Login</h2>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">EmailId</span>
+            </div>
+            <input
+              type="text"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Password</span>
+            </div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+          <div className="card-actions justify-end">
+            <button
+              className="btn btn-primary mx-auto my-4"
+              onClick={onClickLogin}
+            >
+              LogIn
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
